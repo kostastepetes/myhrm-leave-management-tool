ab0353c0860f3c2999c8e0879e3b8733c3e538e0
kostastepetes <st1064973@ceid.upatras.gr>
2024-09-28 19:31:04 +0300

    initial commit

--- phpdb.sql
+++ phpdb.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.0.2
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Mar 30, 2016 at 12:47 PM
--- Server version: 10.0.17-MariaDB
--- PHP Version: 5.5.30
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
